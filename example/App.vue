f01d60b07812233da7372c3ed990f92d0a0c5ec3
akinocccc <akinochen@foxmail.com>
2024-03-31 00:12:59 +0800

    feat(core): [styled] add `.attrs` api

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import {styled} from "../src/styled";
-
-const StyledDiv = styled('div')`
+import {styled} from "@vue3-styled-components/package";
+const StyledDiv = styled.div.attrs<{class: string}>({class: "test"})`
   width: 400px;
   height: 100px;
   background-color: red;
@@ -10,34 +9,9 @@ const StyledDiv = styled('div')`
   justify-content: space-around;
   align-items: center;
 `;
-const StyledButton = styled('button')`
-  width: 100px;
-  height: 100px;
-  background-color: blue;
-  color: white;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
 
-const StyledSpan = styled.span`
-  width: 100px;
-  height: 100px;
-  background-color: yellow;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
 </script>
 
 <template>
-  <StyledDiv>
-    <div>1221222ww32</div>
-    <div>1221222ww32</div>
-    <div>1221222ww32</div>
-  </StyledDiv>
-  <StyledButton>哈哈哈哈</StyledButton>
-  <button>123</button>
-  <StyledSpan />
+  <StyledDiv as="p"></StyledDiv>
 </template>

d55e953851d4bdd2cf27055d64d44274477910ba
akinocccc <akinochen@foxmail.com>
2024-08-16 23:20:43 +0800

    test(keframes): fix incorrect test case

--- core/__tests__/keyframes.test.ts
+++ core/__tests__/keyframes.test.ts
@@ -26,7 +26,16 @@ describe('keyframes', () => {
     `
 
     mount(StyledComponent)
-    expect((document.styleSheets[0].cssRules[0] as CSSKeyframesRule).name).toBe(kf)
-    expect((document.styleSheets[0].cssRules[1] as CSSStyleRule).style['animation-name' as any]).toBe(kf)
+    // Make sure the keyframes are defined
+    const cssRules = document.styleSheets[0].cssRules
+    const keyframesRule = Array.from(cssRules as unknown as CSSKeyframesRule[]).find((rule) => rule?.name === kf)
+
+    expect(keyframesRule).not.toBeUndefined()
+    expect(keyframesRule?.name).toBe(kf)
+
+    // Make sure the animation is applied correctly
+    const styleRule = Array.from(cssRules as unknown as CSSStyleRule[]).find((rule) => rule instanceof CSSStyleRule)
+    expect(styleRule).not.toBeUndefined()
+    expect(styleRule!.style['animation-name' as any]).toBe(kf)
   })
 })

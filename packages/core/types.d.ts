4bbf75519ead220b09dae8cd8bca4a93c2c54a75
akinocccc <akinochen@foxmail.com>
2024-08-18 16:10:25 +0800

    ci(release): add alpha

---
+++ packages/core/types.d.ts
@@ -0,0 +1,20 @@
+// declare module '@vue-styled-components/core' {
+//   export * from './index'
+// }
+// declare module '@vue-styled-components/core' {
+//   export {
+//     styled,
+//     keyframes,
+//     ThemeProvider,
+//     createGlobalStyle,
+//     withAttrs,
+//     isStyledComponent,
+//     isVueComponent,
+//     isTag,
+//     isValidElementType,
+//     useStyledClassName,
+//     css,
+//     cssClass,
+//     DefaultTheme,
+//   } from './index'
+// }

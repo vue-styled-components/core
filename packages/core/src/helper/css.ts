e37e8faff45e1f3f9aa41f78645adb683f266018
Akino <akinochen@foxmail.com>
2025-06-11 01:57:49 +0800

    fix(css): typedef issue(#50)

--- packages/core/src/helper/css.ts
+++ packages/core/src/helper/css.ts
@@ -2,7 +2,7 @@ import type { BaseContext } from '../styled'
 import type { ExpressionType } from '../utils'
 import { insertExpressions } from '../utils'
 
-export function css<T = BaseContext<any>>(
+export function css<T = BaseContext<object & { [key: string]: any }>>(
   strings: TemplateStringsArray,
   ...interpolations: (ExpressionType<T> | ExpressionType<T>[])[]
 ): ExpressionType<T>[] {
